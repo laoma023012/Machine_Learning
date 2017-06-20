614a70935d83eafec2dcb4763b906c28cdabafcf
laoma023012 <16120299@bjtu.edu.cn>
2017-06-20 16:51:43 +0800

    update SVM OOP
    仍然有不稳定的 陷入极小值问题出现，需要在接下来加入回退

--- support vector machine/svm_update.py
+++ support vector machine/svm_update.py
@@ -9,29 +9,113 @@ class Support_Vector_Machine():
     
     # -------------------------------------------------------------------------
     def __init__(self ,  data , label , C , max_iteration ):  
-        
-      self.stop_tag = False  
+      
+      # -------- 终止标识符 ------------- 
+      self.stop_tag = False       
+      # -------- 数据 data --------------
       self.data =  data
+      # -------- 标签 label -------------
       self.label = label
-      self.C = C
+      # ------ C 代表惩罚参数 P 109 ------
+      self.C = C    
+      # -------- 最大迭代次数 ------------           
       self.max_iteration = max_iteration
+      # ----- 拉格朗日乘子 alpha 参数 -----
       self.alpha = np.zeros(data.shape[0])
+      # -----------偏置 b ---------------
       self.b = 0 
+      # ----------- 每一次 search 中 最优的 alpha-1—index -------
       self.alpha_1_index = None
+      # ----------- 每一次 search 中 最优的 alpha-2-index -------
       self.alpha_2_index = None
+      # ------------ 保存每个数的 function E 的值 -----------------------
       self.E = np.zeros( data.shape[0] )
+      # ------------ 保存 每个数的 w 的值 ------------------------------
       self.w = None
+      # ------------ 保存 TARGET FUNCTION 的值 -------------------------
+      self.TARGET_FUNCTION = None
+      # ------------- 保存最优的 alpha --------------------------
+      self.best_alpha = None
+      # ------------- 保存最优的 b ------------------------------------ 
+      self.best_b = None
+      # -------------- 最优值的更新标识符 -----------------------------
+      self.update = False
+      # --------------最优的 w 值 --------------------------------------
+      self.best_w = None
+      
+  # ---------- Calculate the target function and save best if possible -------- 
+    def Target_function( self ):
+        
+        Target_1 = 0
+        for index_1 in range( self.alpha.shape[0] ):
+            for index_2 in range( self.alpha.shape[0] ):
+                Target_1 = Target_1 + self.alpha[index_1] * self.alpha[index_2] * label[index_1] * label[index_2] * self.kernel( data[index_1] , data[index_2] )
+        
+        Target_2 = 0
+        for index_3 in range( self.alpha.shape[0]):
+            Target_2 = Target_2 + self.alpha[ index_3 ]
+        
+        Target = 0.5 * Target_1 - Target_2 
+        
+        if self.TARGET_FUNCTION == None: 
+           self.TARGET_FUNCTION = Target
+           self.best_alpha = self.alpha
+           self.best_b  = self.b
+           self.update = True
+           
+        elif Target < self.TARGET_FUNCTION: 
+           self.TARGET_FUNCTION = Target
+           self.best_alpha = self.alpha
+           self.best_b =  self.b
+           self.update = True
+        
+        #----------------------- w -------------------------------------------
+        best_w = 0
     
-    # --plot the line with w and b-----------------------------------------------------------
+        for index in range(data.shape[0]):
+           best_w = best_w + label[index] * data[index] * self.best_alpha[index]
+          
+        self.best_w = best_w
+        
+        return    
+ # -------------- plot the best plot figure for svm --------------------------------------
+    def best_plot( self ):
+      plt.figure()
+      x = np.linspace(0, 10, 1000)  
+    
+      w_best = (-1)*float( self.best_w[1]) / ( float( self.best_w[0]) + 0.0001 )
+      #print 'w_2 =',w_2
+      b_best = (-1)*self.best_b / float(self.best_w[1])
+      #print 'b_2 =',b_2
+    
+      y = w_best * x + b_best
+      #print 'y', y
+      plt.plot(x,y)
+      data_1 = []
+      data_2 = []
+    
+      for index in range(data.shape[0]):
+        if label[index] == 1:
+            data_1.append(data[index])
+        if label[index] == -1:
+            data_2.append(data[index])
+            
+      data_1 = np.array(data_1)
+      data_2 = np.array(data_2)
+      plt.scatter(data_1[:,0],data_1[:,1])
+      plt.scatter(data_2[:,0],data_2[:,1])
+      return
+  
+ # --plot the line with w and b-----------------------------------------------------------    
     def plot_line( self ):
      
       plt.figure()
       x = np.linspace(0, 10, 1000)  
     
       w_2 = (-1)*float( self.w[1]) / ( float( self.w[0]) + 0.0001 )
-      print 'w_2 =',w_2
+      #print 'w_2 =',w_2
       b_2 = (-1)*self.b / float(self.w[1])
-      print 'b_2 =',b_2
+      #print 'b_2 =',b_2
     
       y = w_2 * x + b_2
       #print 'y', y
@@ -76,7 +160,8 @@ class Support_Vector_Machine():
         sum = 0     
         for index in range(data.shape[0]):
             sum = sum + self.alpha[index] * label[index] * self.kernel(data[index] , data[certain_index])   
-            
+        
+       # print 'type b', self.b
         sum = sum + self.b   
         
         return sum  
@@ -134,9 +219,9 @@ class Support_Vector_Machine():
         self.alpha[self.alpha_1_index]  = alpha_1_new
         self.alpha[self.alpha_2_index]  = alpha_2_new
         
-        print 'alpha-1-new index',self.alpha_1_index,'alpha-2-new index',self.alpha_2_index
-        print 'alpha-1-new',alpha_1_new,'alpha-2-new',alpha_2_new
-        print 'alpha-update',self.alpha
+        #print 'alpha-1-new index',self.alpha_1_index,'alpha-2-new index',self.alpha_2_index
+        #print 'alpha-1-new',alpha_1_new,'alpha-2-new',alpha_2_new
+        #print 'alpha-update',self.alpha
         
         b1_new = - self.function_E( self.alpha_1_index ) \
                  - label[self.alpha_1_index] * self.kernel( data[self.alpha_1_index] , data[self.alpha_1_index]) \
@@ -152,12 +237,12 @@ class Support_Vector_Machine():
                  * ( alpha_2_new - self.alpha[self.alpha_2_index] ) \
                  + self.b
                  
-        print ' b1_new ',b1_new,' b2_new ',b2_new
+        #print ' b1_new ',b1_new,' b2_new ',b2_new
         b_new = ( b1_new + b2_new ) / 2
         
         self.b = b_new
         
-        print 'b-update',b_new
+        #print 'b-update',b_new
         
         return
         
@@ -181,12 +266,15 @@ class Support_Vector_Machine():
         support_vector = np.array( support_vector )
         point_other = np.array( point_other )
         
-        print 'support_vector',support_vector
-        print 'point_other',point_other
+        #print 'support_vector',support_vector
+        #print 'point_other',point_other
+        #support_vector_bias = abs(support_vector - 1)
+        #support_vector_index = np.argsort( support_vector_bias )
         
         # ------------------------优先选择支持向量------------------------------
         if support_vector.shape[0] != 0 :
            alpha_1_index = random.choice(support_vector)
+           #alpha_1_index = support_vector_index[support_vector_index.shape[0] - 1]
            self.alpha_1_index = alpha_1_index
         
         if support_vector.shape[0] == 0 and point_other.shape[0] != 0 :
@@ -197,7 +285,7 @@ class Support_Vector_Machine():
             self.stop_tag = True
             self.alpha_1_index = None
         
-        #print 'alpha-1-index',self.alpha_1_index
+        print 'alpha-1-index',self.alpha_1_index
         return
         # ---------------------------------------------------------------------
     
@@ -207,16 +295,18 @@ class Support_Vector_Machine():
     def search_best_alpha_2( self ):
         #self.E_value_update()
         E1 = self.E[self.alpha_1_index]
-        E = []
+        E_gap = []
         #print 'E_series',self.E
         # ----------------- Calculate the E value ---------------------------
         for index in range( data.shape[0] ):
-            E.append( E1 - self.E[index] ) 
+            E_gap.append( E1 - self.E[index] ) 
             
         # --------------- Search the largest E value ------------------------
-        print 'original_E',E
-        index = np.argsort( abs(np.array(E)) )
+        #print 'original_E',E
         
+        #print 'E',E
+        index = np.argsort( abs(np.array(E_gap)) )
+        print 'E_change',E_gap[index.shape[0] - 1]
        
         print 'index',index
         
@@ -236,6 +326,9 @@ class Support_Vector_Machine():
        Iter = 0  
        #print 'Iter',Iter
        while  ( self.stop_tag == False ) and ( Iter < self.max_iteration ) :
+          # -------- update -------------- 
+          self.update = False
+          
           print ' '
           print 'E-value-update'
           self.E_value_update()
@@ -252,6 +345,12 @@ class Support_Vector_Machine():
           print 'calculate the param w'
           self.param_w()
           
+          #print 'Target_function'
+          #self.Target_function()
+          
+          #print 'best_plot'
+          #self.best_plot()
+          
           print 'plot line'
           self.plot_line()
           
@@ -263,16 +362,16 @@ if __name__=='__main__':
     #data = np.array([[1,2],[2,4],[3,3],[6,6],[8,5],[9,9],[10,14]])
     #label = np.array([1, 1 , 1 ,-1,-1,-1,-1])
     #C = 0.2
-    data = np.array([[1,2],[2,4],[3,3],[6,6],[8,5],[9,9],[10,14]])
-    label = np.array([1, 1 , 1 ,-1,-1,-1,-1])
-    C = 0.002
-    Max_iterations = 200
+    data = np.array([[1,2],[2,4],[3,3],[5,5],[6,6],[8,5],[9,9],[10,14]])
+    label = np.array([1, 1 , 1 , 1, -1 , -1 , -1 , -1 ])
+    C = 200
+    Max_iterations = 20
     #-------------------主函数测试----------------------------------------
     A = Support_Vector_Machine( data , label , C , Max_iterations)
     
     #--------------function_G 函数测试---------------------------------
-    C_1 = A.function_G( 1 )
-    print C_1
+    #C_1 = A.function_G( 1 )
+    #print C_1
     
     #---------------function train -------------------------------------
     A.train()
